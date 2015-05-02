e082bf0d9cac13ff204e588ee161067b2b193938
Reza Jatnika <rezajatnika@gmail.com>
2015-05-02 12:31:08 +0700

    Add full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  # Title helper
+  def full_title(page_title = '')
+    base_title = 'Shouter'
+    page_title.empty? ? base_title : "#{page_title} | #{base_title} "
+  end
 end
